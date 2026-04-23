c14c0c2dc0695e774ea8ab76345547375adb3cd4
Willhelmus Juaness Patudang <willyjuaness@gmail.com>
2026-04-23 09:53:51 +0800

    Perbaikan css tidak terbaca

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,14 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
+    public function boot(): void
+    {
+        // 2. Tambahkan kode ini agar selalu pakai HTTPS di production
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
+    }
+
     /**
      * Bootstrap any application services.
      */
